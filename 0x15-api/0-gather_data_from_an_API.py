2b2c71544dd426e001fea1f63d2be5477804c15d
KofiFloki <jeremiah.martey@yahoo.com>
2023-07-24 14:02:11 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,31 @@
-#!/usr/bin/env python3
-'''
-Using https:// jsonplaceholder . typicod e. com
-returns info about employee T O D O progress
-Implemented using recursion
-'''
- import requests , sys
- from functools import reduce
- from operator import attrgetter
- API = " https:// jsonplaceholder . typicode. com"  # REST API url
- def get(url):  return requests . get(url) . json () # GET request
- def main():  # main function
-     id = sys . argv[ :2 ]
-     id = reduce(attrgetter(' id '), id ) # get employee id
-     res1 = get(' {} / users / {} ' . format ( API , id ) ) # get user info
-     res2 = get(' {} / todos ' . format ( API ) ) # get todos
-     uname = res1[' name '] ; todos = res2
-     td = [ todo for todo in todos if todo[' userId '] == id and todo[' completed '] ] # filter todos
-     tn = [ todo[' title '] for todo in td ] ; print( ' Employee {} is 
-             done with t a s k s( {}/{} ):' . format(uname , len(td) , len(todos )))
-         for t in tn : print('\t {}' . form a t(t)) # print todos done
- if __ name__ = = '__ main__ ': main ( )
+#!/usr/bin/python3
+"""
+Uses READ API for a given employee ID,
+Returns information about the employee TODO list
+progress
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """prevents from executing when imported"""
+    all_users = requests.get("https://jsonplaceholder.typicode.com/users")
+    for user in all_users.json():
+        if user.get('id') == int(argv[1]):
+            Username = (user.get("name"))
+            break
+    Completed = 0
+    Total = 0
+    Title = []
+    tasks = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for task in tasks.json():
+        if task.get("userId") == int(argv[1]):
+            Total += 1
+            if task.get("completed") is True:
+                Completed += 1
+                Title.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(Username, Completed,
+                                                          Total))
+    for task in Title:
+        print("\t {}".format(task))
