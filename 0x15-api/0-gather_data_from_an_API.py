61c77109cc52041dee3f49f4204b32b4dda8f2eb
KofiFloki <jeremiah.martey@yahoo.com>
2023-07-24 13:50:30 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,22 @@
 #!/usr/bin/env python3
 '''
-This program uses the READ API to retrieve information
-about an employees TODOs. The program takes one argument, an
-employee ID. Once the ID is provided, the program will
-retrieve the information from the API and print it to the
-console.'''
-
-## Import necessary modules
-import json
-import requests
-
-## Define function to retrieve employee information
-def get_employee_information(employee_ID):
-'''This function retrieves employee information from
-the READ API using the provided employee_ID.'''
-
-## Create the request URL
-request_url = f'https://api.mycompany.net/v1/employees/{employee_ID}'
-
-## Make the request
-response = json.loads(requests.request('GET', request_url).content)
-return response
-
-## Define function to print employee information
-def print_employee_information(employee):
-'''This function prints employee information to
-the console.'''
-print('Employee ID:', employee['employeeID'])
-print('Name:', employee['name'])
-print('Department:', employee['department'])
-print('Title:', employee['title'])
-
-## Get employee information from user
-employee_ID = input('Enter employee\'s ID: ')
-employee = get_employee_information(employee_ID)
-
-## Print employee information to console
-print_employee_information(employee)
+Using https:// jsonplaceholder . typicod e. com
+returns info about employee T O D O progress
+Implemented using recursion
+'''
+ import requests , sys
+ from functools import reduce
+ from operator import attrgetter
+ API = " https:// jsonplaceholder . typicode. com"  # REST API url
+ def get(url):  return requests . get(url) . json () # GET request
+ def main():  # main function
+     id = sys . argv[ :2 ]
+     id = reduce(attrgetter(' id '), id ) # get employee id
+     res1 = get(' {} / users / {} ' . format ( API , id ) ) # get user info
+     res2 = get(' {} / todos ' . format ( API ) ) # get todos
+     uname = res1[' name '] ; todos = res2
+     td = [ todo for todo in todos if todo[' userId '] == id and todo[' completed '] ] # filter todos
+     tn = [ todo[' title '] for todo in td ] ; print( ' Employee {} is 
+             done with t a s k s( {}/{} ):' . format(uname , len(td) , len(todos )))
+         for t in tn : print('\t {}' . form a t(t)) # print todos done
+ if __ name__ = = '__ main__ ': main ( )
