df9c3dbdf6c59c86073ec64c5a03a1a585c6459b
KofiFloki <jeremiah.martey@yahoo.com>
2023-07-24 15:14:59 -0700

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Using https://jsonplaceholder.typicode.com
+gathers data from API and exports it to JSON file
+Implemented using recursion
+"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    """prevents from executing when imported"""
+    all_users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = []
+    for user in all_users.json():
+        users.append((user.get('id'), user.get("username")))
+    Title = []
+    tasks = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for task in tasks.json():
+        Title.append((task.get("userId"), task.get("completed"),
+                     task.get("title")))
+
+    task_dict = {}
+    for employee in users:
+        task_list = []
+        for item in Title:
+            if item[0] == employee[0]:
+                task_list.append({"task": item[2], "completed": item[1],
+                                 "username": employee[1]})
+        task_dict[str(employee[0])] = task_list
+
+    json_file = "todo_all_employees.json"
+    with open(json_file, "w") as myfile:
+        json.dump(task_dict, myfile)
