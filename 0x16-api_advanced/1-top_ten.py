62ee7d3b3e0a570bf0306bd7e5cfe3e2acd7be52
KofiFloki <jeremiah.martey@yahoo.com>
2023-08-08 16:32:44 -0700

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,19 +8,14 @@ import requests
 
 
 def top_ten(subreddit):
-    # Get the JSON response from the Reddit API.
-    response = requests.get(
-        "https://api.reddit.com/r/{}/hot/.json".format(subreddit),
-        headers={"User-Agent": "my-app/0.0.1"},
-    )
-    # Check if the response was successful.
-    if response.status_code != 200:
-        print("Invalid subreddit")
-        return
-    # Get the JSON response from the Reddit API.
-    response_json = response.json()
-    # Get the titles of the first 10 hot posts.
-    titles = [post["title"] for post in response_json["data"]["children"][:10]]
-    # Print the titles of the first 10 hot posts.
-    for title in titles:
-        print(title)
+    """a function that prints the first 10 hot posts"""
+
+    url = "https://www.reddit.com/r/{}.json?limit=10".format(subreddit)
+    response = requests.get(url, headers={'User-Agent': 'My User Agent'})
+    if response.status_code == 200:
+        result = response.json()
+        top = result['data']['children']
+        for items in top:
+            print(items['data']['title'])
+    else:
+        print(None)
