fb20c14f440cd5a5cb65fcc2215b655c8b70ccec
KofiFloki <jeremiah.martey@yahoo.com>
2023-08-08 17:03:13 -0700

    Count It

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,36 +1,48 @@
 #!/usr/bin/python3
-"""
-Provides a recursive function that queries the Reddit API
-parses the title of all hot articles and prints a sorted
-count of given keywords.
-"""
-
-from collections import Counter
+[200~#!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.
+'''
 import requests
-import json
-def count_words(subreddit, word_list):
-    """
-    Queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).
-    Args:
-        subreddit (str): The subreddit to query.
-        word_list (list): A list of keywords to search for.
-    """
-    url = "https://www.reddit.com/r/{}.json?limit=10".format(subreddit)
-    response = requests.get(url, headers={'User-Agent': 'My User Agent'})
-    if response.status_code == 200:
-        result = response.json()
-        titles = [item['data']['title'] for item in result['data']['children']]
-        counts = {word: 0 for word in word_list}
-        for title in titles:
-            for word in word_list:
-                if word.lower() in title.lower():
-                    counts[word] += 1
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_counts:
-            print(word, count)
-    else:
-        print('Invalid subreddit.')
-if __name__ == '__main__':
-    subreddit = input('Enter a subreddit: ')
-    word_list = input('Enter a list of words, separated by spaces: ').split()
-    count_words(subreddit, word_list)
+def sort_histogram(histogram={}):
+    '''Sorts and prints the given histogram.
+    '''
+    histogram = list(filter(lambda kv: kv[1], histogram))
+    histogram_dict = {}
+    for item in histogram:
+        if item[0] in histogram_dict:
+            histogram_dict[item[0]] += item[1]
+        else:
+            histogram_dict[item[0]] = item[1]
+    histogram = list(histogram_dict.items())
+    histogram.sort(
+        key=lambda kv: kv[0],
+        reverse=False
+    )
+    histogram.sort(
+        key=lambda kv: kv[1],
+        reverse=True
+    )
+    res_str = '\n'.join(list(map(
+        lambda kv: '{}: {}'.format(kv[0], kv[1]),
+        histogram
+    )))
+    if res_str:
+        print(res_str)
+def count_words(subreddit, word_list, histogram=[], n=0, after=None):
+    '''Counts the number of times each word in a given wordlist
+    occurs in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'hot'
+    limit = 30
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.
